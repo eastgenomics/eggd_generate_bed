26dcf18203bb4c9cf0911941a5f3d0022c8b7f34
jethror1 <rainford1995@gmail.com>
2020-09-30 17:51:06 +0100

    working shell script for app

--- src/eggd_generate_bed.sh
+++ src/eggd_generate_bed.sh
@@ -3,23 +3,42 @@
 
 main() {
 
-    echo "Value of panel: '$panel'"
-    echo "Value of exons_nirvana: '$exons_nirvana'"
-    echo "Value of nirvana_genes2transcripts: '$nirvana_genes2transcripts'"
-    echo "Value of gemini_panels: '$gemini_panels'"
+    # download all input files and move to home
+    dx-download-all-inputs
+    find ~/in -type f -name "*" -print0 | xargs -0 -I {} mv {} ./
 
+    # check either panel name or manifest and sample file given
+    if [[ -z $panel ]] || [[ -z $manifest_name ]] && [[ -z $sample_file_name ]]; then
+        echo "No panel or manifest and sample identifying file found. Exiting now."
+        exit 1
+    fi
+
+    # check if panel string given, if not get from manifest and sample name
+    if [[ -z $panel ]]; then
+        # get the same ID from the first part of the file name
+        sample=$(echo $sample_file_name | cut -d "_" -f 1)
+
+        # get the panel(s) from the sample entry in the manifest
+        panel=$(grep -w $sample ~/$manifest_name | cut -f 2 | sort | uniq | awk '{print}' ORS=', ')
+
+        echo "Sample ID used: " $sample
+        echo "Using panel(s) from manifest: " $panel
+    fi
+
+    pip3 install pandas==0.24.2
+
+    # run in empty our dir incase a .bed file is used as input for uploading output bed file
+    mkdir ./out && cd ./out
+
+    # generate bed file    
+    python3 ~/generate_bed.py -p "$panel" -e ~/$exons_nirvana_name -g ~/$gemini_panels_name -t ~/$nirvana_genes2transcripts_name
     
-    dx download "$exons_nirvana" -o exons_nirvana
-    dx download "$nirvana_genes2transcripts" -o nirvana_genes2transcripts
-    dx download "$gemini_panels" -o gemini_panels
+    echo "Output bed file: "
+    ls
+    
+    bed_file=./*bed
+    output_file=$(dx upload $bed_file --brief)
 
-    pip3 install pandas
+    dx-jobutil-add-output bed_file "$output_file" --class=file
 
-    python3 generate_bed.py -p $panel -e $exons_nirvana_name -g $gemini_panels_name -t $nirvana_genes2transcripts_name
-
-    bed_file=./*.bed
-
-    bed_file=$(dx upload bed_file --brief)
-
-    dx-jobutil-add-output bed_file "$bed_file" --class=file
 }
