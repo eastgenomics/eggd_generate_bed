165f9908acc5e4c3824fe4028e2e123f9e239499
jethror1 <rainford1995@gmail.com>
2020-10-06 15:08:22 +0100

    bug fixes

--- src/eggd_generate_bed.sh
+++ src/eggd_generate_bed.sh
@@ -7,22 +7,26 @@ main() {
     dx-download-all-inputs
     find ~/in -type f -name "*" -print0 | xargs -0 -I {} mv {} ./
 
+    echo "panel: "
+    echo "$panel"
+    echo "files"
+    ls
+
     # check either panel name or manifest and sample file given
-    if [[ -z $panel ]] || [[ -z $manifest_name ]] && [[ -z $sample_file_name ]]; then
-        echo "No panel or manifest and sample identifying file found. Exiting now."
-        exit 1
-    fi
-
-    # check if panel string given, if not get from manifest and sample name
-    if [[ -z $panel ]]; then
+    if [[ -z "$panel" ]]; then
+        if [[ -z "$manifest_name" ]] && [[ -z "$sample_file_name" ]]; then
+            echo "No panel or manifest and sample identifying file found. Exiting now."
+            exit 1;
+        else
+        # manifest and sample file given, get panel from manifest by sample name
         # get the same ID from the first part of the file name
-        # sample=$(echo $sample_file_name | cut -d "_" -f 1)
-        sample=$(grep -oP "^[a-zA-Z0-9]*" <<< $sample_file_name)
+        sample=$(grep -oP "^[a-zA-Z0-9]*" <<< "$sample_file_name")
         # get the panel(s) from the sample entry in the manifest
-        panel=$(grep -w $sample ~/$manifest_name | cut -f 2 | sort | uniq | awk '{print}' ORS=', ')
+        panel=$(grep -w $sample ~/"$manifest_name" | cut -f 2 | sort | uniq | awk '{print}' ORS=', ')
 
-        echo "Sample ID used: " $sample
-        echo "Using panel(s) from manifest: " $panel
+        echo "Sample ID used: $sample"
+        echo "Using panel(s) from manifest: $panel"
+        fi
     fi
 
     pip3 install pandas==0.24.2
@@ -31,13 +35,13 @@ main() {
     mkdir ./out && cd ./out
 
     # generate bed file    
-    python3 ~/generate_bed.py -p "$panel" -e ~/$exons_nirvana_name -g ~/$gemini_panels_name -t ~/$nirvana_genes2transcripts_name
+    python3 ~/generate_bed.py -p "$panel" -e ~/"$exons_nirvana_name" -g ~/"$gene_panels_name" -t ~/"$nirvana_genes2transcripts_name"
 
     echo "Done, uploading BED file."
 
-    bed_file=./*bed
+    bed_file=$(find .  -name "*37*.bed" -o -name "*38*.bed")
 
-    output_file=$(dx upload $bed_file --brief)
+    output_file=$(dx upload "$bed_file" --brief)
 
     dx-jobutil-add-output bed_file "$output_file" --class=file
 
