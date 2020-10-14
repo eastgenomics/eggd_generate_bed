72578054ef42d7a6ead430ebf8479782f872e869
jethror1 <rainford1995@gmail.com>
2020-10-14 12:24:32 +0100

    PR changes to shell script

--- src/eggd_generate_bed.sh
+++ src/eggd_generate_bed.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # eggd_generate_bed 1.0.0
 
+set -euxo pipefail
+
 main() {
 
     # download all input files and move to home
     dx-download-all-inputs
     find ~/in -type f -name "*" -print0 | xargs -0 -I {} mv {} ./
 
-    echo "panel: "
-    echo "$panel"
-    echo "files"
-    ls
+    # install python packages from included wheels
+    pip install packages/pytz-* packages/numpy-* packages/pandas-*
 
     # check either panel name or manifest and sample file given
     if [[ -z "$panel" ]]; then
@@ -25,11 +25,10 @@ main() {
         panel=$(grep -w $sample ~/"$manifest_name" | cut -f 2 | sort | uniq | awk '{print}' ORS=', ')
 
         echo "Sample ID used: $sample"
-        echo "Using panel(s) from manifest: $panel"
         fi
     fi
 
-    pip3 install pandas==0.24.2
+    echo "Using panel(s): $panel"
 
     # run in empty out dir incase a .bed file is used as input for uploading output bed file
     mkdir ./out && cd ./out
@@ -37,9 +36,9 @@ main() {
     # generate bed file    
     python3 ~/generate_bed.py -p "$panel" -e ~/"$exons_nirvana_name" -g ~/"$gene_panels_name" -t ~/"$nirvana_genes2transcripts_name"
 
-    echo "Done, uploading BED file."
+    bed_file=$(find . -name "*37*.bed" -o -name "*38*.bed")
 
-    bed_file=$(find .  -name "*37*.bed" -o -name "*38*.bed")
+    echo "Done, uploading BED file: $bed_file"
 
     output_file=$(dx upload "$bed_file" --brief)
 
