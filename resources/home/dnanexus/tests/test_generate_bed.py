56027d28d58d7edccb812b3deae270137ad9244c
AmanahLewis-Wade <amanah.lewis-wade@nhs.net>
2026-04-30 17:01:32 +0100

    update test script

--- resources/home/dnanexus/tests/test_generate_bed.py
+++ resources/home/dnanexus/tests/test_generate_bed.py
@@ -309,7 +309,7 @@ class TestGenerateBed:
     Method to test headers are added as metadata and not column names 
     """
    
-    def test_header_in_first_line_bed():
+    def test_header_in_first_line_bed(self):
         """Method to test the genrate bed function can generate the same output as the expected be fille
         """
 
