a7ee56fcd8491bb380ff61be100b68f02bb6a4a7
AmanahLewis-Wade <amanah.lewis-wade@nhs.net>
2026-05-18 11:22:03 +0100

    update test_generate_bed.py with additional expected bed file case

--- resources/home/dnanexus/tests/test_generate_bed.py
+++ resources/home/dnanexus/tests/test_generate_bed.py
@@ -419,3 +419,41 @@ class TestGenerateBed:
         res = filecmp.cmp(test_bed_file ,expected_bed_file, shallow=False)       
         
         assert res , ("Files are not the same")
+    
+    def test_bed_with_additional_regions_and_flank(self):
+        """
+        Method to test the generate bed function can generate the same output as the expected bed file with additional regions and flank
+        
+        """
+        # make additional regions dataframe
+        add_regions_file=f"{TEST_DATA_DIR}/test_additional_regions_generate_bed_v1.3.1.tsv"
+        additional_regions=gb.read_to_df(
+        file_name=add_regions_file,
+        sep="\t",
+        required_headers=["chromosome", "start", "end", "gene_panel","transcript"] )
+
+        # run generate bed function and save output to generate_bed_output_file
+        gb.generate_bed(exons=self.read_in_exons_file(f"{TEST_DATA_DIR}/test_exons_generate_bed_v1.3.1.tsv"),
+        transcripts=["NM_080050.4",	
+                     "NM_010000.2",	
+                     "NM_010000.3",	
+                     "NM_181758.1"],
+        panels=["R50.1_Early onset dementia"],
+        genes=["HGNC:10000",
+               "HGNC:12600",
+	           "HGNC:44506",
+	           "HGNC:93010"],
+        genome_build="_b38.bed",
+        output_prefix=f"{TEST_DATA_DIR}/test_beds/test_R50.1_Early_onset_dementia_with_additional_regions_and_flank",
+        flank=400,
+        additional_regions=additional_regions)
+        
+        # get expected and test file paths
+
+        expected_bed_file=f"{TEST_DATA_DIR}/expected_beds/test_expected_bed_file_with_additional_regions_and_flank.bed"
+        test_bed_file=f"{TEST_DATA_DIR}/test_beds/test_R50.1_Early_onset_dementia_with_additional_regions_and_flank_b38.bed"
+
+        # compare expected bed file with bed file generate by gb.generate_bed()
+        res = filecmp.cmp(test_bed_file ,expected_bed_file, shallow=False)       
+        
+        assert res , ("Files are not the same")
